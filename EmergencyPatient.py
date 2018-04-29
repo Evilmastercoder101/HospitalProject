f474b6116508859cd7f7a7962a2fecece1145c5b
EvilMasterCoder101 <jessevw@live.nl>
2018-04-29 20:01:21 +0200

    Now returns dictionary of information with data and fatalities of accidents

--- EmergencyPatient.py
+++ EmergencyPatient.py
@@ -3,7 +3,56 @@
 
 
 import csv
+from copy import deepcopy
 allrows = []
+
+def checkbyday(listofdata):
+    # listofdata is the original copy, datalist is the deep copy
+    datalist = deepcopy(listofdata)
+
+    firstday = datalist[0][0]
+    # first check if there are no days missing:
+    # 0, 1, 2, 3, 6, 10, 11, 12, 13, 14, 15, 16, 17
+    # keep the above indices
+    tokeep = [0, 1, 2, 3, 6]
+    # date, time, borough, zip, loc
+    fatalities = [10, 11, 12, 13, 14, 15, 16, 17]
+    # injured, killed, injured, killed, injured, killed, injured, killed,
+
+    elemlist = []
+    daysdict = {}
+    daysdoubledict = {}
+    tempdicto = {}
+    othertempdicto = {}
+    otherelemlist = []
+    daysdict2 = {}
+    for elem in datalist:
+        if elem[0] == firstday:
+
+            elemlist.append([elem[i] for i in tokeep])
+            elemlist.append([elem[j] for j in fatalities])
+            othertempdicto["data"] = [elem[i] for i in tokeep]
+            tempdicto["fatalities"] = [elem[j] for j in fatalities]
+
+            otherelemlist.append(othertempdicto)
+            otherelemlist.append(tempdicto)
+
+        daysdict[elem[0]] = elemlist
+        daysdict2[elem[0]]= otherelemlist
+    print(daysdict2)
+    #for entry in datalist:
+
+
+    print("###########")
+    print(daysdict)
+
+
+
+
+
+
+
+
 
 with open('NYPD_Motor_Vehicle_Collisions.csv', 'r') as f:
     reader = csv.reader(f)
@@ -21,3 +70,7 @@ hospitalxy = (x,y)
 for row in allrows:
     print(row)
     print("\n")
+# date, time, borough, zip, lat, long, latlongloc, onstreet, cross street, offstreet, injured, killed, injured, killed, injured, killed, injured, killed, factor, factor, factor
+print(checkbyday(allrows))
+
+# 0, 1, 2, 3, 6, 10, 11, 12, 13, 14, 15, 16, 17
