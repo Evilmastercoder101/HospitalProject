90230a797e1504559f77d51338583de0e96fa52b
EvilMasterCoder101 <jessevw@live.nl>
2018-04-30 20:08:50 +0200

    help help help xD

--- EmergencyPatient.py
+++ EmergencyPatient.py
@@ -7,6 +7,113 @@ from datetime import *
 from copy import deepcopy
 allrows = []
 
+def countperfifteen(listofkeys, totalvictimtimeslot):
+    finalslotdicto = {}
+    currenttimeslots = []
+    hours = []
+    minutes = []
+    for firstelem in listofkeys:
+        hours = []
+        minutes = []
+        print(firstelem)
+        boole = True
+        boolemins = True
+        i = 0
+        while boole:
+            hours.append(firstelem[i])
+            if firstelem[i] == ":":
+                boole = False
+            i += 1
+        j = len(firstelem)-1
+        while boolemins:
+            print(j)
+            minutes.append(firstelem[j])
+            if firstelem[j] == ":":
+                boolemins = False
+            j -= 1
+        print(hours)
+        hours.pop()
+        newhours = int(''.join(hours))
+        print(newhours)
+        print(hours)
+        print(minutes)
+        minutes.pop()
+        minutes.reverse()
+        print(minutes)
+        newminutes = int(''.join(minutes))
+        timenow = time(newhours, newminutes, 0)
+
+        currenttimeslots.append(timenow)
+    print(currenttimeslots)
+
+
+
+    for slot in currenttimeslots:
+        print(slot)
+    a = datetime(year=2017, month=1, day=1, minute=0)
+    for i in range(0,20*4):
+        print("THIS IS THE ITERATION")
+        print(i)
+        listofslots = []
+        b = a + timedelta(minutes=15.0)  # days, seconds, then other fields.
+        print(a, b)
+        # move 15 mins
+        a = b
+        groupedslots = []
+        for indexer in range(0, 15):
+            groupedslots.append(a + timedelta(minutes = float(indexer)))
+        for gslot in groupedslots:
+            # make them in hh mm ss format
+            gslot = time(gslot.hour, gslot.minute, gslot.second)
+
+        for slot in currenttimeslots:
+            temptime = time(slot.hour, slot.minute, slot.second)
+            for gslot in groupedslots:
+                if temptime.hour == gslot.hour and temptime.minute == gslot.minute:
+                    print("YAYAYAYAYAY")
+                    print(temptime, gslot)
+                    listofslots.append(temptime)
+        # TODO Make the stuff the right object since now time objects vs not time objcts as kyes
+        finalslotdicto[(a,b)] = [totalvictimtimeslot[slot] for slot in listofslots]
+    print(finalslotdicto)
+    for key in finalslotdicto:
+        print("hey")
+        for subkey in finalslotdicto[key]:
+            print(subkey)
+
+
+
+
+
+    print("#################################")
+    for slot in groupedslots:
+        print(slot)
+
+
+
+
+
+
+
+
+
+    print(b.time())
+
+    print(a)
+    print(b)
+
+def countvictimspertimeslot(nestedlist):
+    sum = 0
+
+
+            # injured killed injured killed injured killed
+            # find the amount of people injured
+
+    for j in range(0, len(nestedlist), 2):
+        # this is where we count the victims: take all the injuries and sum them. TODO we can also see the type of accident, and from there decide the treatment time
+        sum += int(nestedlist[j])
+    return sum
+
 def samedate(date, datetuple):
     currentmonth = int(''.join([date[i] for i in range(0, 2)]))
     currentday = int(''.join([date[i] for i in range(3, 5)]))
@@ -112,8 +219,54 @@ hospitalxy = (x,y)
 
 # date, time, borough, zip, lat, long, latlongloc, onstreet, cross street, offstreet, injured, killed, injured, killed, injured, killed, injured, killed, factor, factor, factor
 finaldaysdict = checkbyday(allrows)
+
+# data, fatal list, data, fatal list, etc...
+victimpertimeslotdicto = {}
+# loop over the days (key)
+timeslot = ""
+totalvictimtimeslot = {}
 for key in finaldaysdict:
-    print(key, finaldaysdict[key])
+#    countvictimspertimeslot(finaldaysdict[key])
+    # loop over the accidents (nestedlist)
+    for index in range(0, len(finaldaysdict[key])):
+        if index%2 == 0:
+            print("first list xD")
+            print(finaldaysdict[key][index+1])
+            print("second list xDD")
+            print(finaldaysdict[key][index])
+            print(countvictimspertimeslot(finaldaysdict[key][index+1]))
+            timeslot = finaldaysdict[key][index][0][1]
+            count = countvictimspertimeslot(finaldaysdict[key][index+1])
+            print("Timeslot")
+            print(timeslot)
+
+            if timeslot not in totalvictimtimeslot:
+                totalvictimtimeslot[timeslot] = count
+            else:
+                totalvictimtimeslot[timeslot] += count
+
+
+            if timeslot not in victimpertimeslotdicto:
+                victimpertimeslotdicto[timeslot] = []
+                victimpertimeslotdicto[timeslot].append((countvictimspertimeslot(finaldaysdict[key][index+1]), finaldaysdict[key][index], finaldaysdict[key][index+1]))
+            else:
+                victimpertimeslotdicto[timeslot].append((countvictimspertimeslot(finaldaysdict[key][index+1]), finaldaysdict[key][index], finaldaysdict[key][index+1]))
+
+
+# victimpertimeslotdicto gives every accident and its data, given a key of a timeslot
+# totalvictimtimeslot gives the total amount of patients injured at a certain timeslot
+sum = 0
+for timeslot in totalvictimtimeslot:
+    if totalvictimtimeslot[timeslot] > 12:
+        print(timeslot)
+print(victimpertimeslotdicto["14:20"])
+print(totalvictimtimeslot["14:20"])
+templist = list(totalvictimtimeslot.keys())
+templist.sort()
+print(templist)
+countperfifteen(templist, totalvictimtimeslot)
+
+
 
 
 # 0, 1, 2, 3, 6, 10, 11, 12, 13, 14, 15, 16, 17
